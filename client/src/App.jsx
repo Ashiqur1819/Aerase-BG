91c8836e51a0a642c2b92363c6c083cbc5f92e97
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2026-02-20 19:36:55 +0600

    Done footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import AppRoutes from './router/AppRoutes'
-import Navbar from './components/Navbar'
-
+import React from "react";
+import AppRoutes from "./router/AppRoutes";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className='min-h-screen bg-slate-50'>
-        <Navbar />
+    <div className="min-h-screen bg-slate-50">
+      <Navbar />
       <AppRoutes />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
